b405761a095cdaae304caadb1299eff6c0e9943f
Mohibbulla Munshi <mohib.agu@gmail.com>
2024-09-21 23:52:40 +0600

    student factory, seeder added

--- backend/database/migrations/2024_09_21_081248_create_students_table.php
+++ backend/database/migrations/2024_09_21_081248_create_students_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('first_name', 100); // First Name
             $table->string('last_name', 100); // Last Name
             $table->string('email', 100)->unique(); // Email (unique)
-            $table->string('phone', 15)->nullable(); // Phone (optional)
+            $table->string('phone', 25)->nullable(); // Phone (optional)
             $table->string('nationality', 50)->nullable(); // Nationality (optional)
             $table->date('birthdate')->nullable(); // Birthdate (optional)
             $table->text('address')->nullable(); // Address (optional)
