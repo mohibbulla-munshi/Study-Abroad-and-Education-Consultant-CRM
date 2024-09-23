565c76afd5b95b0080f3ead70c9b5357e6a17a3d
Mohibbulla Munshi <mohib.agu@gmail.com>
2024-09-23 17:25:21 +0600

    Telescope, StudentController complete and checked by postman

--- backend/app/Models/Student.php
+++ backend/app/Models/Student.php
@@ -8,4 +8,23 @@ use Illuminate\Database\Eloquent\Model;
 class Student extends Model
 {
     use HasFactory;
+    
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'email',
+        'phone',
+        'nationality',
+        'birthdate',
+        'address',
+    ];
+
+    // Specify the primary key name
+    protected $primaryKey = 'student_id';
+
+    // Optional: Disable auto-incrementing if your primary key isn't auto-incrementing (though in this case it is)
+    public $incrementing = true;
+
+    // Optional: Specify the primary key type if it's not an integer (e.g., if you use UUIDs)
+    protected $keyType = 'int';
 }
